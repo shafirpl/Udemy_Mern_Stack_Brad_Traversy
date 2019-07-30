7908b655549f6cfdf2dd48e2fbe7f63d3190b8b7
Shafi Hoque <shafihoque@Shafis-MacBook-Air.local>
2019-07-29 22:55:31 -0700

    WOrking on auth

---
+++ DEVCONNECTOR/middleware/auth.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+/*
+* Remember from middleware functions in node.js
+* that they take three things, req,res and next
+* if something passes, then the next thing happens
+* which in most cases transfers the control to 
+* another function
+*
+* x-auth-token: https://stackoverflow.com/questions/39017297/what-is-difference-between-x-auth-token-vs-authorisation-headers-which-is-prefer
+* 
+*/
+module.exports = (req,res,next) => {
+    // Get token from the header
+    const token = req.header('x-auth-token');
+
+    // check if no token
+
+    if(!token){
+        // 401 is unathorized access error http code
+        res.status(401).json({msg:"No token, authorization denied"});
+
+    }
+
+    // if there is token, verify it
+    try{
+        // console.log(token);
+        // decoding the token
+        const decoded = jwt.verify(token, config.get("jwtSecret"));
+
+        req.user = decoded.user;
+        next();
+    } catch(err){
+        // this will happen if the token is not valid
+        // res.send(err);
+        res.status(401).json({msg:"Token is not valid"});
+    }
+}
--- DEVCONNECTOR/routes/api/auth.js
+++ DEVCONNECTOR/routes/api/auth.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
+const User = require ('../../models/Users');
 /*
 * @route GET api/auth
 * @description: Test route
 * @access Public
+* so recall from node js lessions,
+* we define middleware in a seperate file, and
+* then import it and then put it in the routes,
+* the next function in the middleware will pass
+* the controls to this function if the middleware is 
+* successful
 */
-router.get('/', (req, res) => {
-    res.send('Auth route')
+router.get('/',auth, async (req,res) => {
+    try{
+        /*
+        * select("-password") will leave off the password
+
+        * since we were passing the id in the payload in jwt
+        * when we were sending it to generate the jwt, and so after
+        * generating the token in the server
+        * and getting the token, and after decoding,
+        * we can grab the id. Also since remember if auth passes,
+        * it will run the next function, which will in turn 
+        * execute this funciton, so this function will have access 
+        * to that req, and we did req.uder = decoded.user so
+        * by doing req.user.id, we would have access to the id
+        * 
+        */
+        const user = await User.findById(req.user.id).select("-password");
+        res.json(user);
+
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send("server error");
+    }
 });
 
 module.exports = router;
