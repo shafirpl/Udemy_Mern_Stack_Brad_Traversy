3a853b2bc0d95552ad9e98fb967a98d9c78ddeb2
shafirpl <shafi.rpl@gmail.com>
2019-08-17 02:57:14 -0700

    Completed get all profile stuff

--- DEVCONNECTOR/routes/api/profile.js
+++ DEVCONNECTOR/routes/api/profile.js
@@ -10,37 +10,44 @@ const User = require("../../models/Users");
  * @description: Get current user profile
  * @access Private
  */
-router.get("/me", auth, async (req, res) => {
-    try {
-        /*
-         * https://mongoosejs.com/docs/populate.html
-         * So Populate is a way, after executing a query, in which our case finding the
-         * user with the id, add more stuff/data to that query. So in our populate method,
-         * the first parameter tells the function from which collection/data set we will be grabbing the data,
-         * and the second option tells us what data we will be grabbing.
-         *
-         * Usually we use it on data sets which has refs to other datasets. For our case, the profile
-         * model has ref to users, so we are using that ref as first parameter
-         */
-        const profile = await Profile.findOne({ user: req.user.id }).populate(
-            "user",
-            ["name", "avatar"]
-        );
 
-        if (!profile) {
-            return res.status(400).json({ msg: "There is no profile for this user" });
-        }
-        res.json(profile);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
+router.get("/me", auth, async (req, res) => {
+  try {
+    /*
+     * https://mongoosejs.com/docs/populate.html
+     * So Populate is a way, after executing a query, in which our case finding the
+     * user with the id, add more stuff/data to that query. So in our populate method,
+     * the first parameter tells the function from which collection/data set we will be grabbing the data,
+     * and the second option tells us what data we will be grabbing.
+     *
+     * Usually we use it on data sets which has refs to other datasets. For our case, the profile
+     * model has ref to users, so we are using that ref as first parameter
+     */
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
     }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 /*
  * @route POST api/profile
  * @description: Create or Update an User Profile
  * @access Private
+ */
+
+/*
+ * https://www.udemy.com/mern-stack-front-to-back/learn/lecture/10055200?start=585#questions
+ * Watch from 15:32
+ * This summarizes what we are doing here
  */
 
 /*
@@ -51,139 +58,171 @@ router.get("/me", auth, async (req, res) => {
  * Then we check if the user actually provided the status and skills
  */
 router.post(
-    "/",
+  "/",
+  [
+    auth,
     [
-        auth,
-        [
-            check("status", "Status is required")
-                .not()
-                .isEmpty(),
-            check("skills", "Skills is required")
-                .not()
-                .isEmpty()
-        ]
-    ],
-    async (req, res) => {
-                          /*
-                           * if we have errors from any one of the check methods, it can
-                           * be retrived using validationResult(req) which returns
-                           * us an array
-                           * so if there is no error, that array should be empty
-                           * For more information:
-                           * https://express-validator.github.io/docs/validation-result-api.html
-                           */
-                          const errors = validationResult(
-                            req
-                          );
-                          if (!errors.isEmpty()) {
-                            return res
-                              .status(400)
-                              .json({
-                                errors: errors.array()
-                              });
-                          }
-                          // array destructuring to grab all required data from the request body
-                          const {
-                            company,
-                            website,
-                            location,
-                            bio,
-                            status,
-                            githubusername,
-                            skills,
-                            youtube,
-                            facebook,
-                            twitter,
-                            instagram,
-                            linkedin
-                          } = req.body;
+      check("status", "Status is required")
+        .not()
+        .isEmpty(),
+      check("skills", "Skills is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    /*
+     * if we have errors from any one of the check methods, it can
+     * be retrived using validationResult(req) which returns
+     * us an array
+     * so if there is no error, that array should be empty
+     * For more information:
+     * https://express-validator.github.io/docs/validation-result-api.html
+     */
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+    // array destructuring to grab all required data from the request body
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin
+    } = req.body;
 
-                          // Build profile object
+    // Build profile object
 
-                          /*
-                           * this means if the field exits, add that field, for example
-                           * if the company field exists, add the company filed
-                           * Since this {} means an object, at the end of the day profileFields object will look like
-                           * this
-                           * profileFields{
-                           *   company:something,
-                           *   website: something,
-                           *   location: something}
-                           * And so on
-                           */
-                          const profileFields = {};
-                          profileFields.user =
-                            req.user.id;
-                          if (company)
-                            profileFields.company = company;
-                          if (website)
-                            profileFields.website = website;
-                          if (location)
-                            profileFields.location = location;
-                          if (bio)
-                            profileFields.bio = bio;
-                          if (status)
-                            profileFields.status = status;
-                          if (githubusername)
-                            profileFields.githubusername = githubusername;
+    /*
+     * this means if the field exits, add that field, for example
+     * if the company field exists, add the company filed
+     * Since this {} means an object, at the end of the day profileFields object will look like
+     * this
+     * profileFields{
+     *   company:something,
+     *   website: something,
+     *   location: something}
+     * And so on
+     */
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
 
-                          /*
-                           * So the skills will be comma separated values, and we
-                           * want to have an array for it. Split breaks a string to array
-                           * Here we wanna split at every comma and take that value to an
-                           * array element
-                           * https://www.tutorialrepublic.com/faq/how-to-convert-comma-separated-string-into-an-array-in-javascript.php
-                           * 
-                           * Now we don't want whitespaces, we use trim for it
-                           * https://www.w3schools.com/jsref/jsref_trim_string.asp
-                           * So trim gets rid of whitespaces
-                           * Now lets say we had something like anna,shafi,     evana,jason,    someoneelse
-                           * Now all of them will be an array element, but then evana and someoneelse will have unnecessary whitespaces
-                           * So in array it will look like [anna,shafi,    evana,jason,   someoneelse] 
-                           * We don't want the whitespace. So we run a loop for each element using map, and then for each
-                           * element we use trim method to get rid of the whitespaces
-                           */
-                          if (skills) {
-                            profileFields.skills = skills
-                              .split(",")
-                              .map(skill =>
-                                skill.trim()
-                              );
-                          }
+    /*
+     * So the skills will be comma separated values, and we
+     * want to have an array for it. Split breaks a string to array
+     * Here we wanna split at every comma and take that value to an
+     * array element
+     * https://www.tutorialrepublic.com/faq/how-to-convert-comma-separated-string-into-an-array-in-javascript.php
+     *
+     * Now we don't want whitespaces, we use trim for it
+     * https://www.w3schools.com/jsref/jsref_trim_string.asp
+     * So trim gets rid of whitespaces
+     * Now lets say we had something like anna,shafi,     evana,jason,    someoneelse
+     * Now all of them will be an array element, but then evana and someoneelse will have unnecessary whitespaces
+     * So in array it will look like [anna,shafi,    evana,jason,   someoneelse]
+     * We don't want the whitespace. So we run a loop for each element using map, and then for each
+     * element we use trim method to get rid of the whitespaces
+     */
+    if (skills) {
+      profileFields.skills = skills.split(",").map(skill => skill.trim());
+    }
 
-                          // Build social object
-                          profileFields.social = {};
-                          if (youtube)
-                            profileFields.social.youtube = youtube;
-                          if (twitter)
-                            profileFields.social.twitter = twitter;
-                          if (facebook)
-                            profileFields.social.facebook = facebook;
-                          if (linkedin)
-                            profileFields.social.linkedin = linkedin;
-                          if (instagram)
-                            profileFields.social.instagram = instagram;
+    // Build social object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
 
-                          try {
-                            let profile =await Profile.findOne({user: req.user.id });
-                            if(profile){
-                              // update existing profile if profile exists
-                              /*
-                              * https://mongoosejs.com/docs/tutorials/findoneandupdate.html
-                              */
-                              profile = await Profile.findOneAndUpdate({ user: req.user.id},{$set: profileFields}, {new: true});
-                              return res.json(profile);
-                            }
-                            // if profile doesn't exist, create new one
-                            profile = new Profile(profileFields);
-                            await Profile.save();
-                            return res.json(profile);
-                          } catch (error) {
-                            console.error(error.message);
-                            res.status(500).send("Server Error");
-                          }
-                        }
-                        //hrllo
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        // update existing profile if profile exists
+        /*
+         * https://mongoosejs.com/docs/tutorials/findoneandupdate.html
+         */
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+      // if profile doesn't exist, create new one
+      profile = new Profile(profileFields);
+      await profile.save();
+      return res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+  //hrllo
 );
+
+/*
+ * @route GET api/profile
+ * @description: GET Profile by user id
+ * @access Public
+ */
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+/*
+ * @route GET api/profile/user/:user_id
+ * @description: GET All Profiles
+ * @access Public
+ */
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    /*
+     * https://www.udemy.com/mern-stack-front-to-back/learn/lecture/10055206?start=60#overview
+     * From 9:24
+     */
+    if (error.kind == "ObjectId") {
+      return res.status(400).json({
+        msg: "Profile not found"
+      });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
