1169f74807d14f33762f51038910baf62a3ef716
shafirpl <shafi.rpl@gmail.com>
2020-01-23 02:33:10 -0800

    Create profile action created

--- DEVCONNECTOR/client/src/actions/profile.jsx
+++ DEVCONNECTOR/client/src/actions/profile.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import {setAlert} from './alert.jsx';
+import { setAlert } from './alert.jsx';
 
-import {GET_PROFILE, PROFILE_ERROR} from './types.jsx';
+import { GET_PROFILE, PROFILE_ERROR } from './types.jsx';
 
 // get current user's profile
 
-export const getCurrentProfile = ()=> async dispatch => {
+export const getCurrentProfile = () => async dispatch => {
     try {
         const res = await axios.get('/api/profile/me');
         dispatch({
@@ -15,8 +15,48 @@ export const getCurrentProfile = ()=> async dispatch => {
     } catch (error) {
         dispatch({
             type: PROFILE_ERROR,
-            payload: {msg: error.response.statusText,
-            status: error.response.status}
+            payload: {
+                msg: error.response.statusText,
+                status: error.response.status
+            }
         })
+    }
+}
+
+// create or update profile
+export const createProfile = (fromData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            header: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const res = await axios.post('/api/profile', fromData, config);
+        dispatch({
+            type: GET_PROFILE,
+            action: res.data
+        });
+
+
+        dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', "success"));
+
+        if (!edit) {
+            /*
+            * we cannot use redirect in an action, so we are using push method from history
+            */
+            history.push('/dashboard');
+        }
+    } catch (error) {
+        const errors = error.response.data.errors;
+        errors.forEach(error =>
+            dispatch(setAlert(error.msg, "danger", 5000))
+        );
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: error.response.statusText,
+                status: error.response.status
+            }
+        });
     }
 }
--- DEVCONNECTOR/client/src/components/profile-forms/CreateProfile.jsx
+++ DEVCONNECTOR/client/src/components/profile-forms/CreateProfile.jsx
@@ -1,8 +1,11 @@
 import React, { useState, Fragment } from 'react'
 import PropTypes from 'prop-types';
+import {Link,withRouter} from 'react-router-dom';
 import { connect } from 'react-redux';
+import {createProfile} from '../../actions/profile.jsx';
 
 const CreateProfile = props => {
+    const {createProfile, history} = props;
     const [formData, setFormData] = useState({
         company: "",
         website: "",
@@ -36,6 +39,11 @@ const CreateProfile = props => {
 
     const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value })
 
+    const onSubmit = e => {
+        e.preventDefault();
+        createProfile(formData,history);
+    }
+
     const [displaySocialInputs, toggleSocialInputs] = useState(false);
     return (
         <Fragment>
@@ -45,7 +53,7 @@ const CreateProfile = props => {
                 your profile stand out
         </p>
             <small>* = required field</small>
-            <form className="form">
+            <form className="form" onSubmit = {e => onSubmit(e)}>
                 <div className="form-group">
                     <select name="status" value={status} onChange={e => onChange(e)}>
                         <option value="0">* Select Professional Status</option>
@@ -213,7 +221,10 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
-
+    createProfile: PropTypes.func.isRequired,
 }
 
-export default connect()(CreateProfile)
+
+
+// to use history object, we need to wrap the component withRouter
+export default connect(null,{createProfile})(withRouter(CreateProfile))
