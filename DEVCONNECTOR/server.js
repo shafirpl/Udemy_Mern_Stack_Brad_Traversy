da8ddf78948183b4143888e905c4dd2952cc8fe3
shafirpl <shafi.rpl@gmail.com>
2020-02-22 08:38:08 -0800

    Final touch up before deployment

--- DEVCONNECTOR/server.js
+++ DEVCONNECTOR/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 // routes variables
 const userRoute = require('./routes/api/users');
@@ -22,7 +23,7 @@ connectDB();
 */
 app.use(express.json({extended: false}));
 
-app.get("/", (req,res) => {res.send('API Running')});
+// app.get("/", (req,res) => {res.send('API Running')});
 
 // Define Routes
 
@@ -32,7 +33,14 @@ app.use('/api/profile', profileRoute);
 app.use('/api/posts', postsRoute);
 
 
-
+// serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static('client/build'));
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+    });
+}
 
 /*
 * This means when the app will be deployed to heroku, it will
